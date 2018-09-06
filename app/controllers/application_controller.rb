fb7f98aca891816f914c2d88ec7f29feca5bfe32
Jocie Moore <hello@jociemoore.com>
2018-09-05 20:38:54 -0700

    Restrict user actions when not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user, :require_login
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def require_login
+    if !current_user
+      flash["error"] = "You must be logged in to do that."
+      redirect_back fallback_location: root_path
+    end
   end
 end
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,6 @@
 class TicketsController < ApplicationController
   before_action :set_ticket, only: [:show, :edit, :update, :destroy]
+  before_action :require_login, except: [:index, :show]
 
   def index
     @tickets = Ticket.all
