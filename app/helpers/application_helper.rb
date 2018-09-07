c1297d40686733ca85c4237e48c7d4066daaa4fa
Jocie Moore <hello@jociemoore.com>
2018-09-07 10:00:33 -0700

    Add timestamps to comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,50 @@ module ApplicationHelper
 
     "#{count} #{count > 1 ? 'errors' : 'error'} prohibited this #{item} from being saved:"
   end
+
+  def how_long_ago(time)
+    @difference = Time.now.utc - time
+    phrase = ""
+
+    if less_than_60_seconds?
+      phrase = "less than a minute ago"
+    elsif less_than_1_hour?
+      minutes = @difference / 60
+      phrase = "#{minutes.to_i} #{singular_minutes? ? 'minute' : 'minutes'} ago"
+    elsif less_than_1_day?
+      hours = @difference / 3600
+      phrase = "about #{hours.to_i} #{singular_hours? ? 'hour' : 'hours'} ago"
+    else
+      days = @difference / 86400
+      phrase = "#{days.to_i} #{singular_days? ? 'day' : 'days'} ago"
+    end
+
+    phrase
+  end
+
+  private
+
+  def less_than_60_seconds?
+    @difference < 60
+  end
+
+  def less_than_1_hour?
+    @difference < 3600
+  end
+
+  def less_than_1_day?
+    @difference < 86400
+  end
+
+  def singular_minutes?
+    @difference < 120
+  end
+
+  def singular_hours?
+    @difference < 86400 && ((@difference / 3600).to_i == 1)
+  end
+
+  def singular_days?
+    (@difference / 86400).to_i == 1
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,8 @@ class Comment < ActiveRecord::Base
   belongs_to :creator, foreign_key: :creator, class_name: 'User'
 
   validates :body, presence: true
+
+  def updated?
+    !self.created_at.eql?(self.updated_at)
+  end
 end
