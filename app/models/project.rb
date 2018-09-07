77c190f6e1a6c94707928601b608d5a76fee5d32
Jocie Moore <hello@jociemoore.com>
2018-09-06 21:58:58 -0700

    Make open tickets and total tickets columns dynamic

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,12 @@ class Project < ActiveRecord::Base
   has_many :tickets, dependent: :destroy
 
   validates :name, presence: true
+
+  def total_open_tickets
+    open_tickets = self.tickets.select do |ticket|
+      ticket[:status] != "fixed"
+    end
+
+    open_tickets.size
+  end
 end
