f2b3f180d48bfd573e6126b4c64ff5388b11b775
Jocie Moore <hello@jociemoore.com>
2018-09-06 21:47:19 -0700

    Delete dependent records

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ActiveRecord::Base
   has_many :ticket_tags
-  has_many :tickets, through: :ticket_tags
+  has_many :tickets, through: :ticket_tags, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
 
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,7 +3,7 @@ class Ticket < ActiveRecord::Base
   belongs_to :creator, foreign_key: 'creator', class_name: 'User'
   belongs_to :assignee, foreign_key: 'assignee', class_name: 'User'
   has_many :ticket_tags
-  has_many :tags, through: :ticket_tags
+  has_many :tags, through: :ticket_tags, dependent: :destroy
   has_many :comments, dependent: :destroy
 
   validates :name, presence: true
