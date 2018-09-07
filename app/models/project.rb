d3142b9915783353d5ae10741769457c22648a41
Jocie Moore <hello@jociemoore.com>
2018-09-07 11:33:49 -0700

    Refactor syntax and formatting

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
   has_many :tickets, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
   def total_open_tickets
     open_tickets = self.tickets.select do |ticket|
